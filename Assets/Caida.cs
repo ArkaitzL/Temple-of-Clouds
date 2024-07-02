13f4889f2ffd6188010493f9bff0be42a15c71da
Arkaitz <127892239+ArkaitzL@users.noreply.github.com>
2024-07-02 14:39:40 +0200

    -

---
+++ Assets/Caida.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caida : MonoBehaviour
+{
+    [SerializeField] GameObject entero, roto;
+
+    Collider colicion;
+    bool terminado;
+
+    private void Start()
+    {
+        colicion = GetComponent<Collider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !terminado)
+        {
+            colicion.enabled = false;
+            terminado = true;
+
+            entero.SetActive(false);
+            roto.SetActive(true);
+        }
+    }
+}
